bc368e92ff1cadf601bb4e2a57c4e3b7c3ecd118
mayankpcp7 <jariwalmayank123@gmail.com>
2023-07-26 12:38:32 +0530

    root

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './second.css';
-import { Footer } from 'react-bootstrap/lib/Modal';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./second.css";
+import { Footer } from "react-bootstrap/lib/Modal";
 function App() {
   return (
     <>
-      <Footer/>
-  </>
+      <NavBar />
+      <Footer />
+    </>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
-import { Nav } from 'react-bootstrap'
+import React from "react";
+import { Nav } from "react-bootstrap";
+import nav_logo from "../assets/images/png/nav_logo.png";
 const NavBar = () => {
   return (
     <>
-    <Nav>
-        <div>
-            <div className="logo"></div>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Solutions</a></li>
-                <li><a href="#">Cases</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Contact Us</a></li>
+      <Nav>
+        <div className="d-flex justify-content-between w-100 container py-3 align-items-center">
+          <div className="logo py-1">
+            <img src={nav_logo} alt="logo" />
+          </div>
+          <div className="d-flex align-items-center">
+            <ul className="d-flex gap-5 py-1 mb-0">
+              <li>
+                <a href="you.com">Home</a>
+              </li>
+              <li>
+                <a href="you.com">Solutions</a>
+              </li>
+              <li>
+                <a href="you.com">Cases</a>
+              </li>
+              <li>
+                <a href="you.com">About Us</a>
+              </li>
+              <li>
+                <a href="you.com">Blog</a>
+              </li>
+              <li>
+                <a href="you.com">Contact Us</a>
+              </li>
             </ul>
+            <div className="ps-5"><button>Get in Touch</button></div>
+          </div>
         </div>
-    </Nav>
+      </Nav>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
