e91d834e18a5117f1bc876cdce73fe9e28bd41e6
Your Name <sumitkhan.dhana@gmail.com>
2023-07-26 11:28:02 +0530

    add assets

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         <div>
             <div className="logo"></div>
             <ul>
-                <li><a href=""></a></li>
+                <li><a href="#">Home</a></li>
             </ul>
         </div>
     </Nav>
