c8df8d8b88cc3f65bfe1be8775991d9e4c42f6b4
mayankpcp7 <jariwalmayank123@gmail.com>
2023-07-26 12:24:20 +0530

    pull root

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './second.css';
+import { Footer } from 'react-bootstrap/lib/Modal';
 function App() {
   return (
-  <>
+    <>
+      <Footer/>
   </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg_white">
+        <div className="d-flex align-items-center  ">
+          <h2 className="text-center ff_nunito color_black ">Get in Touch</h2>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
